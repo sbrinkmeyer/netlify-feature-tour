1b8fde2a4e7cea9fa8d0c534802919819c319c82
s.brinkmeyer <scott.brinkmeyer@shell.com>
2023-10-20 15:30:58 -0500

    adding a rediredirect and a function

---
+++ netlify/functions/hello-world.js
@@ -0,0 +1,7 @@
+exports.handler = async () => {
+    const mySecret = process.env.MY_SECRET;
+    return {
+        statusCode: 200,
+        body: `Hello World! checkout this ${mySecret}`,
+    };
+};
